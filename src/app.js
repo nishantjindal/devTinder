a07f1355a962067ec50f19f729c285d20df0916e
Nishant Jindal <jindaln3120@gmail.com>
2026-04-05 14:49:49 +0530

    added razorpay integration

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const paymentRouter = require("./routes/payment");
 const cors = require("cors");
 require("./utils/cronjobs");
 
@@ -22,7 +23,7 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-
+app.use("/", paymentRouter);
 connectDB()
   .then(() => {
     console.log("Database coneection established");
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+    console.log("✅ DB connected");
+  } catch (err) {
+    console.error("❌ DB ERROR:", err.message);
+  }
 };
 
 module.exports = connectDB;
